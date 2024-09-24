c3253b7d5b5fd3a95d9a78577d26055a0ec779ae
AbdulTur <aturonov@mun.ca>
2024-09-24 20:36:06 -0230

    TT-11 Created Frontend and Backend Structure, and init a react app

---
+++ frontend/src/App.js
@@ -0,0 +1,15 @@
+import './styles/App.css';
+import HelloWorld from './components/HelloWorld';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <h1>Welcome to TripTailor</h1>
+        <HelloWorld />
+      </header>
+    </div>
+  );
+}
+
+export default App;
