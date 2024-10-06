85070e803557958f51e686383fc490a20bdb2ae6
LeoGilbertt <lgilbert@mun.ca>
2024-10-06 15:21:52 -0230

    Removed event service fixed models folder

--- backend/itinerary-service/internal/models/itinerary.go
+++ backend/itinerary-service/internal/models/itinerary.go
@@ -1,14 +1,11 @@
 package models
 
-// Import the models package from event-service
-import "backend/event-service/models"
-
 type Itinerary struct {
 	Description string
 	Name        string
 	Location    string
 	Tags        []string
-	Events      []models.Event
+	Events      []Event
 	Date        string
 }
 
