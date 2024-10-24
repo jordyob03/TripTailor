4345da8bdd520669c53b7eaa7f3e48532344ab13
Waleed Mannan Khan Sherwani <waleedk005@gmail.com>
2024-10-23 22:42:41 -0230

    Fixed itinerary-service issues

--- backend/services/itinerary-service/internal/handlers/CreateItin.go
+++ backend/services/itinerary-service/internal/handlers/CreateItin.go
@@ -4,21 +4,23 @@ import (
 	"database/sql"
 	"fmt"
 	"net/http"
-	"time"
 
 	"github.com/gin-gonic/gin"
 	models "github.com/jordyob03/TripTailor/backend/services/itinerary-service/internal/models"
 )
 
 type CreateItinRequest struct {
-	Name      string   `json:"name"`
-	City      string   `json:"city"`
-	Country   string   `json:"country"`
-	Languages []string `json:"languages"`
-	Tags      []string `json:"tags"`
-	Events    []string `json:"events"`
-	PostId    int      `json:"postId"`
-	Username  string   `json:"username"`
+	Name        string   `json:"name"`
+	City        string   `json:"city"`
+	Country     string   `json:"country"`
+	Title       string   `json:"title"`
+	Description string   `json:"description"`
+	Price       float64  `json:"price"`
+	Languages   []string `json:"languages"`
+	Tags        []string `json:"tags"`
+	Events      []string `json:"events"`
+	PostId      int      `json:"postId"`
+	Username    string   `json:"username"`
 }
 
 func CreateItin(dbConn *sql.DB) gin.HandlerFunc {
@@ -32,16 +34,17 @@ func CreateItin(dbConn *sql.DB) gin.HandlerFunc {
 		fmt.Printf("Received Itinerary: %+v\n", req)
 
 		itin := models.Itinerary{
-			Name:         req.Name,
-			City:         req.City,
-			Country:      req.Country,
-			Languages:    req.Languages,
-			Tags:         req.Tags,
-			Events:       req.Events,
-			PostId:       req.PostId,
-			Username:     req.Username,
-			CreationDate: time.Now(),
-			LastUpdate:   time.Now(),
+			Name:        req.Name,
+			City:        req.City,
+			Country:     req.Country,
+			Title:       req.Title,
+			Description: req.Description,
+			Price:       req.Price,
+			Languages:   req.Languages,
+			Tags:        req.Tags,
+			Events:      req.Events,
+			PostId:      req.PostId,
+			Username:    req.Username,
 		}
 
 		itinId, err := models.AddItinerary(dbConn, itin)
